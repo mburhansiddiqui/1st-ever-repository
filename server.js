88c69a42d07c0c3f0592cd8196f029322122a7c3
burhan siddiqui <Burhan>
2018-05-25 16:16:53 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials' )
+
+app.use((req,res,next)=>{
+var now = new Date().toString();
+var log = now+req.method+req.url ;
+console.log(log);
+fs.appendFileSync('server.log' , log + '\n');
+next();
+});
+
+// app.use((req,res,next)=>{
+//   res.render('maintain.hbs');
+// });
+
+hbs.registerHelper('getcurrentyear' , ()=> {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamit' , (text)=> {
+  return text.toUpperCase();
+});
+
+app.get('/', (req,res) => {
+  // res.send('<h1>hello world</h1>');
+  res.send({
+    name: 'Burhan' ,
+    likes : ['biking', 'cities']
+  });
+});
+app.get('/home',(req,res)=>{
+  res.render('home.hbs',{
+    pagetitle: 'About Page',
+    welcomemessage : 'Welcome to my Website'
+  })
+});
+
+app.get('/about' , (req,res) => {                   // res.send('About Page')
+  res.render('about.hbs',{
+  pagetitle: 'About Page',
+});
+  ;
+});
+app.listen(3000);
